08dfeda0540e4a4af1383f64a06b9c1e8d5318eb
dyankastutara <dyankastutara19@gmail.com>
2017-06-15 11:24:39 +0700

    create update delete transactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -22,7 +22,7 @@ module.exports = {
     });
   },
   update: function(req, res) {
-    Transaction.update({ _id: req.id }, {
+    Transaction.update({ _id: req.params.id }, {
       $set: req.body
     }, function(err, result) {
       if (err) {
@@ -34,7 +34,7 @@ module.exports = {
     });
   },
   delete: function(req, res) {
-    Transaction.remove({ _id: req.id }, function (err, result) {
+    Transaction.remove({ _id: req.params.id }, function (err, result) {
       if (err) {
         res.status(500)
         res.send({err: err})
