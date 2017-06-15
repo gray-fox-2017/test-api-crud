42ebb9cb300d9846becd9910b48aaeac4e722604
ridho0 <ridho230994@gmail.com>
2017-06-15 11:43:20 +0700

    fixed update books

--- controllers/books.js
+++ controllers/books.js
@@ -22,16 +22,33 @@ module.exports = {
     });
   },
   update: function(req, res) {
-    Book.update({ _id: req.id }, {
-      $set: req.body
-    }, function(err, result) {
-      if (err) {
-        res.status(500)
-        res.send({err: err})
+    console.log(Object.keys(req.body)[0]);
+    let body = JSON.parse(Object.keys(req.body)[0])
+    Book.findById(req.params.id, (err, data)=>{
+      if(!err){
+        console.log(typeof body);
+        console.log(body.isbn ,body.title ,body.author ,body.category ,body.stock ,body.price);
+        Book.findByIdAndUpdate(req.params.id, {
+          // $set: req.body
+          $set: {
+            isbn: body.isbn || data.isbn,
+            title: body.title || data.title,
+            author: body.author || data.author,
+            category: body.category || data.category,
+            stock: body.stock || data.stock,
+            price: body.price || data.price
+          }
+        }, function(err, result) {
+          if (err) {
+            res.status(500)
+            res.send({err: err})
+          }
+          res.status(200)
+          res.send(result)
+        });
       }
-      res.status(200)
-      res.send(result)
-    });
+    })
+
   },
   delete: function(req, res) {
     Book.remove({ _id: req.id }, function (err, result) {
