547128d0abdfb68b5b5ab0dba94fe45f8d4d8871
ambodalle <ambodalle.st@gmail.com>
2017-06-15 11:02:56 +0700

    add body parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const bodyParser = require('body-parser')
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/api-crud-mongoose');
@@ -8,7 +9,8 @@ var books = require('./routes/books')
 var transactions = require('./routes/transactions')
 
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
 app.use('/books', books);
 app.use('/transactions', transactions);
