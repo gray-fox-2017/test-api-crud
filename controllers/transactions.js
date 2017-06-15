98caba5eb4d637be483b6f6cc041d391da5219e7
parelhutahaean <parel.hutahaean@gmail.com>
2017-06-15 11:30:02 +0700

    Second Commit

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -24,7 +24,7 @@ module.exports = {
     });
   },
   update: function(req, res) {
-    Transaction.update({ _id: req.id }, {
+    Transaction.update({ _id: req.params.id }, {
       $set: req.body
     }, function(err, result) {
       if (err) {
@@ -36,7 +36,7 @@ module.exports = {
     });
   },
   delete: function(req, res) {
-    Transaction.remove({ _id: req.id }, function (err, result) {
+    Transaction.remove({ _id: req.params.id }, function (err, result) {
       if (err) {
         res.status(500)
         res.send({err: err})
