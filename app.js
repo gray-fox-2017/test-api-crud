2ac4028d5bf36d820a1bc1f7d458da117512c63f
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-06-15 10:55:40 +0700

    fixing error dengan status 'cannot read property (push) of undefined)

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const app = express()
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/api-crud-mongoose');
 
 var books = require('./routes/books')
-var transactions = require('./routes/transactions')
+var transactions = require('./routes/transactions');
+
+const app = express();
 
 app.use('/books', books);
 app.use('/transactions', transactions);
 
 app.listen(3000)
+
+module.exports = app;
