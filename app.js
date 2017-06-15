6702afe319822ed7781fc161edd04db9707ba9ff
ambodalle <ambodalle.st@gmail.com>
2017-06-15 10:47:48 +0700

    add module.exports books dan transaction

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ mongoose.connect('mongodb://localhost/api-crud-mongoose');
 var books = require('./routes/books')
 var transactions = require('./routes/transactions')
 
+
+
+
 app.use('/books', books);
 app.use('/transactions', transactions);
 
 app.listen(3000)
+
+module.exports = app;
