957ca0821c69dcf236e42b61209b8b4867990df1
dyankastutara <dyankastutara19@gmail.com>
2017-06-15 10:52:50 +0700

    body-parser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express()
 
 var mongoose = require('mongoose');
@@ -7,7 +8,12 @@ mongoose.connect('mongodb://localhost/api-crud-mongoose');
 var books = require('./routes/books')
 var transactions = require('./routes/transactions')
 
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 app.use('/books', books);
 app.use('/transactions', transactions);
 
-app.listen(3000)
+app.listen(3000, ()=>{
+	console.log("Connected to port 3000")
+})
