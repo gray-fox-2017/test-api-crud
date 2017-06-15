a79f2d9b3bbdc2e54b3dd3bfecea1a26fac4a085
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-06-15 11:07:38 +0700

    error solved - Router.use() requires middleware function but got a Object

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express()
-var router = express.Router();
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/api-crud-mongoose');
+var bodyParser = require('body-parser')
 
 var books = require('./routes/books')
 var transactions = require('./routes/transactions')
 
+
+// app.use('/', routes);
 app.use('/books', books);
 app.use('/transactions', transactions);
 
--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,3 +1,5 @@
+var Transaction = require('../models/transaction')
+
 module.exports = {
   all: function(req, res) {
     Transaction.find(function (err, transactions) {
@@ -11,7 +13,7 @@ module.exports = {
   },
   create: function(req, res) {
     var transaction = new Transaction(req.body);
-    transaction.save(function (err, result) {
+    Transaction.save(function (err, result) {
       if (err) {
         res.status(500)
         res.send({err: err})
@@ -21,7 +23,7 @@ module.exports = {
     });
   },
   update: function(req, res) {
-    Transaction.update({ _id: req.id }, {
+    Transaction.update({ _id: req.params.id }, {
       $set: req.body
     }, function(err, result) {
       if (err) {
@@ -33,7 +35,7 @@ module.exports = {
     });
   },
   delete: function(req, res) {
-    Transaction.remove({ _id: req.id }, function (err, result) {
+    Transaction.remove({ _id: req.params.id }, function (err, result) {
       if (err) {
         res.status(500)
         res.send({err: err})
