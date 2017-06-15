e57295f7e331b0e23811432b4f3852e3f3dda4a0
ambodalle <ambodalle.st@gmail.com>
2017-06-15 11:22:23 +0700

    oke minus create transaction

--- routes/transactions.js
+++ routes/transactions.js
@@ -3,8 +3,8 @@ const router = express.Router();
 var transactionsController = require('../controllers/transactions');
 
 router.get('/', transactionsController.all)
-// router.post('/', transactionsController.create)
-// router.put('/:id', transactionsController.update)
-// router.delete('/:id', transactionsController.delete)
+router.post('/', transactionsController.craete)
+router.put('/:id', transactionsController.update)
+router.delete('/:id', transactionsController.delete)
 
 module.exports = router
